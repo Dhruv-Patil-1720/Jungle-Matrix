a1ecc1c6bb6b4304067c153a706979b9cf3a8b8b
Dhruv Patil <138751440+Dhruv-Patil-1720@users.noreply.github.com>
2023-11-25 22:54:55 +0530

    Add files via upload

--- sound.js
+++ sound.js
@@ -4,3 +4,14 @@ bgsound.loop=true
 bgsound.currentTime=0 
 bgsound.play()
 
+function toggleSound() {
+    const soundButton = document.getElementById("sound-button");
+    
+    if (bgsound.paused) {
+        bgsound.play();
+        soundButton.style.backgroundImage = "url('./assets/sound_on_-removebg-preview\ \(1\).png')";
+    } else {
+        bgsound.pause();
+        soundButton.style.backgroundImage = "url(./assets/sound_off-removebg-preview.png)"; // Image for sound-off state
+    }
+}
